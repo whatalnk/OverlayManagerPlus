170c4654647e688db2b9fea5dbb38fe040b5d813
Wataru NAKAI <watnk0806@gmail.com>
2017-03-15 15:44:31 +0900

    Function add_row

--- SegmentManager.py
+++ SegmentManager.py
@@ -4,10 +4,10 @@ import os.path
 import json
 from ij import IJ, ImagePlus, WindowManager
 from ij.plugin.frame import PlugInFrame
-from ij.gui import Overlay, GenericDialog, Line, PointRoi
+from ij.gui import Overlay, GenericDialog, Line, PointRoi, TextRoi
 from fiji.util.gui import GenericDialogPlus
 import java.lang.Boolean as JBool
-from java.awt import GridLayout
+from java.awt import GridLayout, Font
 from javax.swing import JButton, JPanel, JTable, JScrollPane, BoxLayout
 from javax.swing.table import DefaultTableModel
 from javax.swing.event import TableModelListener, ListSelectionListener
@@ -51,6 +51,8 @@ class SegmentManager(OverlayManagerPlus):
         p2.add(b_open)
         b_add = JButton("Add", actionPerformed = self.add_row)
         p2.add(b_add)
+        b_edit = JButton("Edit", actionPerformed = self.edit_row)
+        p2.add(b_edit)
         b_del = JButton("Delete", actionPerformed = self.delete_row)
         p2.add(b_del)
         b_ren_i = JButton("Rename index", actionPerformed = self.rename_index)
@@ -63,6 +65,22 @@ class SegmentManager(OverlayManagerPlus):
         self.frame.visible = True
 
     def add_row(self, event):
+        selection = self.imp.getRoi()
+        if selection is None or selection.getType() !=5:
+            return
+        self.roi[self.currentIndex+1] = selection
+        label = TextRoi((selection.x1 + selection.x2)/2, (selection.y1 + selection.y2)/2, str(self.currentIndex+1), Font("Consolas", Font.PLAIN, 200))
+        self.text[self.currentIndex+1] = label
+        
+        tblModel = self.tbl.getModel()
+        tblModel.addRow([self.currentIndex+1, selection.getLength()])
+        self.currentIndex += 1
+
+        self.overlay.add(selection)
+        self.overlay.add(label)
+        self.imp.setOverlay(self.overlay)
+
+    def edit_row(self, event):
         pass
 
     def delete_row(self, event):
