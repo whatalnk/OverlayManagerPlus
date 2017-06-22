b74772fecbeae71fe34e9cbc75eb16e0f0bae630
Wataru NAKAI <watnk0806@gmail.com>
2017-06-22 19:29:34 +0900

    See: http://forum.imagej.net/t/jython-isssue-with-if---name-----main--/5544/4

--- OverlayManagerPlus_.py
+++ OverlayManagerPlus_.py
@@ -1,4 +1,8 @@
+from ij import IJ
 from OverlayManagerPlus import OverlayManagerPlus
 
-if __name__ == '__main__':
+if __name__ in ['__main__', '__builtin__']:
+    if __name__ is not '__main__':
+        msg = "Warning: __name__ is  %s" % __name__
+        IJ.log(msg)
     OverlayManagerPlus.OverlayManagerPlus()
